ad57f48e53aea87a75594111e96e0315980e5e6f
pmcgrath17 <48302523+pmcgrath17@users.noreply.github.com>
2019-06-07 05:16:08 -0700

    Add files via upload

--- PS2/PS_2.v
+++ PS2/PS_2.v
@@ -14,7 +14,7 @@
 
 // PROGRAM		"Quartus Prime"
 // VERSION		"Version 18.0.0 Build 614 04/24/2018 SJ Lite Edition"
-// CREATED		"Fri Jun 07 01:17:46 2019"
+// CREATED		"Fri Jun 07 01:55:28 2019"
 
 module PS_2(
 	data,
@@ -33,18 +33,18 @@ output wire	debug;
 output wire	[6:0] ones;
 output wire	[6:0] sixteens;
 
-wire	counter_one;
 wire	[7:0] display;
-wire	DoESthIsWoRK;
 wire	falling_edge_ps2Clock;
 wire	SYNTHESIZED_WIRE_0;
-wire	[10:0] SYNTHESIZED_WIRE_10;
+wire	[10:0] SYNTHESIZED_WIRE_13;
 wire	[11:0] SYNTHESIZED_WIRE_2;
 wire	SYNTHESIZED_WIRE_3;
 wire	SYNTHESIZED_WIRE_4;
 wire	SYNTHESIZED_WIRE_5;
-wire	SYNTHESIZED_WIRE_11;
-wire	SYNTHESIZED_WIRE_9;
+wire	SYNTHESIZED_WIRE_14;
+wire	SYNTHESIZED_WIRE_8;
+wire	SYNTHESIZED_WIRE_15;
+wire	SYNTHESIZED_WIRE_12;
 
 
 
@@ -53,18 +53,18 @@ wire	SYNTHESIZED_WIRE_9;
 data_received	b2v_inst(
 	.data(SYNTHESIZED_WIRE_0),
 	.clk(falling_edge_ps2Clock),
-	.message(SYNTHESIZED_WIRE_10));
+	.message(SYNTHESIZED_WIRE_13));
 
 
 valid	b2v_inst10(
-	.a(SYNTHESIZED_WIRE_10),
-	.works(DoESthIsWoRK));
+	.a(SYNTHESIZED_WIRE_13),
+	.works(SYNTHESIZED_WIRE_5));
 	defparam	b2v_inst10.N = 11;
 
 
 compare	b2v_inst12(
 	.a(SYNTHESIZED_WIRE_2),
-	.gte(counter_one));
+	.gte(SYNTHESIZED_WIRE_14));
 	defparam	b2v_inst12.M = 2600;
 	defparam	b2v_inst12.N = 12;
 
@@ -78,9 +78,9 @@ encounter	b2v_inst13(
 
 assign	falling_edge_ps2Clock =  ~SYNTHESIZED_WIRE_4;
 
-assign	SYNTHESIZED_WIRE_11 = DoESthIsWoRK & counter_one;
+assign	SYNTHESIZED_WIRE_15 = SYNTHESIZED_WIRE_5 & SYNTHESIZED_WIRE_14;
 
-assign	SYNTHESIZED_WIRE_3 =  ~counter_one;
+assign	SYNTHESIZED_WIRE_3 =  ~SYNTHESIZED_WIRE_14;
 
 
 sevenseg	b2v_inst19(
@@ -91,7 +91,7 @@ sevenseg	b2v_inst19(
 sync	b2v_inst2(
 	.d(device_clock),
 	.clk(clock),
-	.q(SYNTHESIZED_WIRE_9));
+	.q(SYNTHESIZED_WIRE_12));
 
 
 sevenseg	b2v_inst20(
@@ -102,32 +102,32 @@ sevenseg	b2v_inst20(
 sync	b2v_inst3(
 	.d(data),
 	.clk(clock),
-	.q(SYNTHESIZED_WIRE_5));
+	.q(SYNTHESIZED_WIRE_8));
 
 
 checker	b2v_inst4(
-	.data(SYNTHESIZED_WIRE_5),
+	.data(SYNTHESIZED_WIRE_8),
 	.clk(clock),
 	.q(SYNTHESIZED_WIRE_0));
 
 
 message	b2v_inst5(
 	.clk(clock),
-	.enable(SYNTHESIZED_WIRE_11),
-	.d(SYNTHESIZED_WIRE_10),
+	.enable(SYNTHESIZED_WIRE_15),
+	.d(SYNTHESIZED_WIRE_13),
 	.q(display));
 	defparam	b2v_inst5.M = 8;
 	defparam	b2v_inst5.N = 11;
 
 
 sync	b2v_inst6(
-	.d(SYNTHESIZED_WIRE_11),
+	.d(SYNTHESIZED_WIRE_15),
 	.clk(clock),
 	.q(debug));
 
 
 checker	b2v_inst7(
-	.data(SYNTHESIZED_WIRE_9),
+	.data(SYNTHESIZED_WIRE_12),
 	.clk(clock),
 	.q(SYNTHESIZED_WIRE_4));
 
